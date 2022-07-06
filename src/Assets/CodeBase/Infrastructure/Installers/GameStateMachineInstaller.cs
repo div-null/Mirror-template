e6ff96d1d6d61048e742689ad4f57d50d27df61f
Optifun <frdima19@gmail.com>
2022-07-06 20:28:07 +0700

    Add Initial LifetimeScope

---
+++ src/Assets/CodeBase/Infrastructure/Installers/GameStateMachineInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Game.CodeBase.Infrastructure.States;
+using VContainer;
+using VContainer.Unity;
+
+namespace Game.CodeBase.Infrastructure.Installers
+{
+    public class GameStateMachineInstaller : IInstaller
+    {
+        private IGameStateMachine _gameStateMachine;
+        public void Install(IContainerBuilder builder)
+        {
+            builder.Register<BootstrapState>(Lifetime.Scoped);
+            builder.Register<PrepareLobbyState>(Lifetime.Scoped);
+            builder.Register<HotPotatoState>(Lifetime.Scoped);
+        }
+    }
+}
---
+++ src/Assets/CodeBase/Infrastructure/Installers/InitialScope.cs
@@ -0,0 +1,39 @@
+﻿using Game.CodeBase.Infrastructure.States;
+using Game.CodeBase.Services.Network;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Game.CodeBase.Infrastructure.Installers
+{
+    public class InitialScope : LifetimeScope
+    {
+        [SerializeField] private AdvancedNetworkManager _advancedNetworkManagerPrefab;
+
+        class EntryPoint : IStartable
+        {
+            private readonly IGameStateMachine _gameStateMachine;
+
+            public EntryPoint(IGameStateMachine gameStateMachine)
+            {
+                _gameStateMachine = gameStateMachine;
+            }
+
+            public void Start()
+            {
+                _gameStateMachine.Enter<BootstrapState>();
+            }
+        }
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterEntryPoint<EntryPoint>();
+            builder.Register<MainInputActions>(Lifetime.Singleton);
+            builder.Register<PlayerProgressData>(Lifetime.Singleton);
+
+            LifetimeScope stateMachineScope = CreateChild(new GameStateMachineInstaller());
+            var gameStateMachine = stateMachineScope.Container.Resolve<IGameStateMachine>();
+            builder.Register(_ => gameStateMachine, Lifetime.Singleton);
+        }
+    }
+}
