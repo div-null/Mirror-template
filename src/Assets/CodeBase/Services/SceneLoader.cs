a5937239004d1d4d1882578bbc60291ec5e637b3
Optifun <frdima19@gmail.com>
2022-07-10 19:03:20 +0700

    Refactor SceneLoader: use UniTask

--- src/Assets/CodeBase/Services/SceneLoader.cs
+++ src/Assets/CodeBase/Services/SceneLoader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,21 +14,28 @@ namespace Game.CodeBase.Infrastructure
         public SceneLoader(ICoroutineRunner coroutineRunner) => _coroutineRunner = coroutineRunner;
 
         public void Load(string name, Action onLoaded = null) => _coroutineRunner.StartCoroutine((LoadScene(name, onLoaded)));
-  
 
-        private IEnumerator LoadScene(string nextScene, Action onLoaded = null)
+        public async UniTask LoadAsync(string name)
+        {
+            await LoadSceneAsync(name);
+        }
+
+
+        private IEnumerator LoadScene(string nextScene, Action onLoaded = null) =>
+            UniTask.ToCoroutine(() => LoadSceneAsync(nextScene, onLoaded));
+
+        private static async UniTask LoadSceneAsync(string nextScene, Action onLoaded = null)
         {
             if (SceneManager.GetActiveScene().name == nextScene)
             {
                 onLoaded?.Invoke();
-                yield break;
+                await UniTask.CompletedTask;
             }
-      
+
             AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(nextScene);
 
-            while (!waitNextScene.isDone)
-                yield return null;
-      
+            await UniTask.WaitUntil(() => waitNextScene.isDone);
+
             onLoaded?.Invoke();
         }
     }
