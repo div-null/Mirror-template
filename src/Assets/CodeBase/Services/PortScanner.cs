d8715d9b60c86904b3b3bb058345121eab828a98
Optifun <frdima19@gmail.com>
2022-07-10 13:54:20 +0700

    Add PortScanner

---
+++ src/Assets/CodeBase/Services/PortScanner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Game.CodeBase.Services
+{
+    public static class PortScanner
+    {
+        /// <summary>
+        /// checks for used ports and retrieves the first free port
+        /// </summary>
+        /// <returns>the free port or 0 if it did not find a free port</returns>
+        public static int GetAvailablePort(int startingPort)
+        {
+            IPEndPoint[] endPoints;
+            List<int> portArray = new List<int>();
+
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            //getting active connections
+            TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+            portArray.AddRange(connections
+                .Where(n => n.LocalEndPoint.Port >= startingPort)
+                .Select(n => n.LocalEndPoint.Port));
+
+            //getting active tcp listners - WCF service listening in tcp
+            endPoints = properties.GetActiveTcpListeners();
+            portArray.AddRange(endPoints
+                .Where(n => n.Port >= startingPort)
+                .Select(n => n.Port));
+
+            //getting active udp listeners
+            endPoints = properties.GetActiveUdpListeners();
+            portArray.AddRange(endPoints
+                .Where(n => n.Port >= startingPort)
+                .Select(n => n.Port));
+
+            portArray.Sort();
+
+            for (int i = startingPort; i < UInt16.MaxValue; i++)
+                if (!portArray.Contains(i))
+                    return i;
+
+            return 0;
+        }
+    }
+}
