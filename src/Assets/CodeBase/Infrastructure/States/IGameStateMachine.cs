1d2a1ad01e72b861a5b7ecb797bc7455efcab84f
Optifun <frdima19@gmail.com>
2022-07-13 15:03:20 +0700

    Fix IGameStateMachine interface

--- src/Assets/CodeBase/Infrastructure/States/IGameStateMachine.cs
+++ src/Assets/CodeBase/Infrastructure/States/IGameStateMachine.cs
@@ -3,5 +3,6 @@
     public interface IGameStateMachine
     {
         void Enter<TState>() where TState : class, IState;
+        void Enter<TState, TPayLoad>(TPayLoad payLoad) where TState : class, IPayloadedState<TPayLoad>;
     }
 }
